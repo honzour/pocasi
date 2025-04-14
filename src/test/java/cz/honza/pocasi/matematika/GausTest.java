b71b5b9a52f27a629bef65cd37f0b239166c9e71
honza <jnem6403@seznam.cz>
2025-04-14 21:20:25 +0200

    testy

---
+++ src/test/java/cz/honza/pocasi/matematika/GausTest.java
@@ -0,0 +1,21 @@
+package cz.honza.pocasi.matematika;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class GausTest {
+
+	@Test
+	void test() {
+		
+		double[][] matice = {{2, 0}, {0, 2}};
+		double[] vektor = {6, 6}; 
+		
+		double[] vysledek = Gaus.solve(matice, vektor);
+		Assertions.assertNotNull(vysledek);
+		Assertions.assertEquals(2, vysledek.length);
+		Assertions.assertEquals(3, vysledek[0]);
+		Assertions.assertEquals(3, vysledek[1]);
+	}
+
+}
