dd55bd14441fed7a6e0136630afff2b67c5259be
honza <jnem6403@seznam.cz>
2025-05-01 20:06:15 +0200

    cary

--- src/main/java/cz/honza/pocasi/gui/PanelRocniTeploty.java
+++ src/main/java/cz/honza/pocasi/gui/PanelRocniTeploty.java
@@ -5,6 +5,7 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Rectangle;
+import java.time.LocalDate;
 import java.util.List;
 
 import cz.honza.pocasi.io.Radek;
@@ -37,14 +38,62 @@ public class PanelRocniTeploty extends PanelSGrafem {
 		}
 	}
 	
+	protected void kresliCaryTeplot(Graphics g) {
+		final Rectangle r = g.getClipBounds();
+		g.setColor(Color.BLACK);
+		for (double y = -10; y < 50; y += 5) {
+			int j = scaleToScreanY(y, r.height);
+			g.drawLine(0, j, r.width - 1, j);
+			final String text = String.valueOf(y) + "°C";
+			g.drawChars(text.toCharArray(), 0, text.length(), 10, j - 10);
+		}
+	}
+	
+	protected String mesic(int m) {
+		switch (m) {
+		case 1: return "Leden";
+		case 2: return "Únor";
+		case 3: return "Březen";
+		case 4: return "Duben";
+		case 5: return "Květen";
+		case 6: return "Červen";
+		case 7: return "Červenec";
+		case 8: return "Srpen";
+		case 9: return "Září";
+		case 10: return "Říjen";
+		case 11: return "Listopad";
+		case 12: return "Prosinec";
+		default: return null;
+		}
+	}
+	
+	protected void kresliCaryMesicu(Graphics g) {
+		final Rectangle r = g.getClipBounds();
+		g.setColor(Color.BLACK);
+		for (int mesic = 1; mesic < 13; mesic++) {
+			double x = LocalDate.of(2025, mesic, 1).getDayOfYear();
+			
+			int i = scaleToScreanX(x, r.width);
+			g.drawLine(i, 0, i, r.height - 1);
+			final String text = mesic(mesic);
+			g.drawChars(text.toCharArray(), 0, text.length(), i + 5, 15);
+		}
+	}
+	
+	protected void kresliKrivku(Graphics g) {
+		final Graphics2D g2 = (Graphics2D) g;
+        g2.setStroke(new BasicStroke(5));
+        g.setColor(Color.RED);
+        kresliJednuFunkci(g, prubehRoku);
+	}
+	
 	@Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
         kresliData(g);
-        final Graphics2D g2 = (Graphics2D) g;
-        g2.setStroke(new BasicStroke(5));
-        g.setColor(Color.RED);
-        kresliJednuFunkci(g, prubehRoku);
+        kresliCaryTeplot(g);
+        kresliCaryMesicu(g);
+        kresliKrivku(g);
 	}
 	
 }
